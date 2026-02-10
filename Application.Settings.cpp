f5116aa1b4cbd1332bb44a900a638d4245a2ac01
rernamp <renamp100@gmail.com>
2026-02-10 21:12:41 +0300

    add missed parsed filed in json

--- Application.Settings.cpp
+++ Application.Settings.cpp
@@ -6,6 +6,7 @@ using json = nlohmann::json;
 
 void from_json(const json& j, ConfigSettings& settings) {
     j.at("targetDeviceProperties").get_to(settings.targetDeviceProperties);
+    j.at("dumpProperties").get_to(settings.dumpProperties);
 }
 
 ConfigSettings parseFromString(const std::string& str) {
